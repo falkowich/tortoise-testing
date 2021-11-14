31f6066cc87800339dcca87aff11f9833af09cd2
Falk <gammalget@gmail.com>
2021-11-14 16:30:35 +0100

    basic db

---
+++ foreign-key.py
@@ -0,0 +1,35 @@
+from tortoise import models, fields, Tortoise, run_async
+
+
+class Event(models.Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    datetime = fields.DatetimeField(null=True)
+
+    class Meta:
+        table = "event"
+
+    def __str__(self):
+        return self.name
+
+
+async def run():
+
+    await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["__main__"]})
+    await Tortoise.generate_schemas()
+
+    event = await Event.create(name="Test")
+    await Event.filter(id=event.id).update(name="Updated name")
+
+    print(await Event.filter(name="Updated name").first())
+    # >>> Updated name
+
+    await Event(name="Test 2").save()
+    print(await Event.all().values_list("id", flat=True))
+    # >>> [1, 2]
+    print(await Event.all().values("id", "name"))
+    # >>> [{'id': 1, 'name': 'Updated name'}, {'id': 2, 'name': 'Test 2'}]
+
+
+if __name__ == "__main__":
+    run_async(run())
